abf526d0bbd9dea384b24bbbb9f714ff67c5753d
Vlad Reznikov <sysadmin@lnmo.ru>
2023-04-06 22:09:07 +0400

    Fix build script

--- build_production.sh
+++ build_production.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir -p /build_out
-cp -r dist/* /build_out/
-sed -E -i "s|RANDOM_NONCE|$(uuid)|g" /build_out/index.html
+cp -r assets/* /build_out/
+sed -E -i "s|RANDOM_NONCE|$RANDOM$RANDOM$RANDOM$RANDOM|g" /build_out/index.html
 elm make --optimize --output=/build_out/main.js src/Main.elm
